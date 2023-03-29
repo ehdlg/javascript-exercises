5204c18f6c61144b78e090afd7e2265beaf7ef97
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-03-29 08:18:49 +0200

    Ejercicio 2 con test pasados con exito

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, repetition) {
+    if(stringToRepeat === ''){
+        return '';
+    }
+    if(repetition < 0){
+        return 'ERROR';
+    }
+    let repeatedString = '';
+    for(let i = 0; i < repetition; i++){
+        repeatedString += stringToRepeat;
+    }
+    return repeatedString;
 
 };
 
