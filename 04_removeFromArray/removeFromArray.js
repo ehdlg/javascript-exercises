613a529c007c94605329bbd73227464c076d1b2b
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-03-29 08:31:10 +0200

    Tests del ejercicio 4 pasados

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  args.forEach((argument) => {
+    let index = array.indexOf(argument);
+    if (index !== -1) {
+      array.splice(array.indexOf(argument), 1);
+    }
+  });
 
+  return array;
 };
 
 // Do not edit below this line
