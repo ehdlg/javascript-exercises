e8c96fc9cde143f2fbc0d93179a19a2482968838
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-03-29 08:42:57 +0200

    Tests del ejercicio 5 completados

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (firstNumber, secondNumber) {
+  if (
+    typeof firstNumber !== "number" ||
+    typeof secondNumber !== "number" ||
+    firstNumber < 0 ||
+    secondNumber < 0
+  ) {
+    return 'ERROR';
+  }
+  let min = firstNumber;
+  let max = secondNumber;
+  if (firstNumber > secondNumber) {
+    min = secondNumber;
+    max = firstNumber;
+  }
 
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
